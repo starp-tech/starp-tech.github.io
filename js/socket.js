e363dd72d6def2a2d55c32f8a1639b7c56600915
Daniel <fiend.of.sound@gmail.com>
2022-12-25 21:05:35 +0400

    feat: add websocket controller and handle for join party

--- js/socket.js
+++ js/socket.js
@@ -1,33 +1,64 @@
+new function() {
+  const apiHost = "www.starpy.me"
+  const partyListUrl = `https://${apiHost}/api/v1/party-list`
+  const partyMediaUrl = (partyId) => 
+    `https://${apiHost}/api/v1/party-media?partyId="${partyId}"`
+  const socketUrl = `wss://${apiHost}/api/v1/socket`
 
-function join(partyId) {
-  // If we are running via wrangler dev, use ws:
-  const socketUrl = "wss://www.starpy.me/api/v1/socket"
-  const username = "anon"
-  const userId = "anon"
+  let currentSocket;
+  let username = "anon"
+  let userId = "anon"
+  let partyList = [];
+  let party;
+  let partyId;
+  let partyMedia;
+  const createSocket = async () => {
+    
+    const ws = new WebSocket(socketUrl);
+
+    ws.addEventListener("open", event => {
+      console.info('ws opened')
+      currentSocket = ws
+      ws.send(JSON.stringify({
+        name: username, 
+        partyId, 
+        userId
+      }));
+    });
+
+    ws.addEventListener("message", event => {
+      let data = JSON.parse(event.data);
+      console.info('new party message', data)
+    });
+
+    ws.addEventListener("close", event => {
+      console.log("WebSocket closed, reconnecting:", event.code, event.reason);
+      createSocket()
+    });
+
+    ws.addEventListener("error", event => {
+      console.log("WebSocket error, reconnecting:", event);
+      // rejoin();
+    });
+  }
+
+  const pressJoinPublic = async () => {
+    console.info("pressJoinPublic start")
+    try {
+      partyList = (await (await fetch(partyListUrl)).json())
+      [party] = partyList.rows
+      partyId = party.id
+      partyMedia = await fetch(partyMediaUrl())
+      await createSocket()
+    } catch(err) {
+      console.info("pressJoinPublic start", err)
+    }
+  }
   
-  const ws = new WebSocket(socketUrl);
-
-  ws.addEventListener("open", event => {
-    currentWebSocket = ws;
-    console.info('ws opened')
-    // Send user info message.
-    ws.send(JSON.stringify({name: username, partyId, userId}));
-  });
-
-  ws.addEventListener("message", event => {
-    let data = JSON.parse(event.data);
-    console.info('new party message', data)
-  });
-
-  ws.addEventListener("close", event => {
-    console.log("WebSocket closed, reconnecting:", event.code, event.reason);
-    join(partyId)
-  });
-
-  ws.addEventListener("error", event => {
-    console.log("WebSocket error, reconnecting:", event);
-    // rejoin();
-  });
+  const button = document.getElementById("joinParty")
+  
+  button.addEventListener("click", (e)=>{
+    e.preventDefault()
+    pressJoinPublic()
+  })
 }
-
-join()
