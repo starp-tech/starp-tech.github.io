d0267d9fe7001893a6f80f3fe7a6c657de38732e
Daniel <fiend.of.sound@gmail.com>
2022-12-25 22:19:01 +0400

    feat: add media playback

--- js/media.js
+++ js/media.js
@@ -40,47 +40,6 @@ const processIncomingFileLink = () => {
 		console.error("processIncomingFileLink err", err)
 	}
 }
-const playMediaLink = (mediaLink) => {
-	try {
-		console.info("playMediaLink", mediaLink)
-		const client = new WebTorrent({
-			// downloadLimit:1000
-		})
-    client.on('error', function (err) {
-      console.error('ERROR: ' + err.message)
-    })
-		const download = () =>  {
-			console.info('playMediaLink on download')
-			client.add(mediaLink, (media) => {
-				try {
-					console.info('playMediaLink on media', media)
-				  const file = media.files.find(function (file) {
-	          return file.name.endsWith(".mp4");
-	        });
-				  console.info('playMediaLink on file', file)
-			    // Render to a <video> element by providing an ID. Alternatively, one can also provide a DOM element.
-	        file.getStreamURL((err, url) => {
-	          console.log("Ready to play!", url);
-	          // document.get
-	        });
-				} catch(err) {
-					console.error("playMediaLink err", err)
-				}
-			})
-		}
-		navigator.serviceWorker.register("sw.min.js").then(reg => {
-		  const worker = reg.active || reg.waiting || reg.installing
-		  function checkState (worker) {
-		    return worker.state === 'activated' && client.loadWorker(worker, download)
-		  }
-		  if (!checkState(worker)) {
-		    worker.addEventListener('statechange', ({ target }) => checkState(target))
-		  }
-		})
-	} catch(err) {
-		console.error("processIncomingFileLink err", err)
-	}
-}
 
 
 if(query 
