ca5c02140c2c4977853c57c722e0f90c29dec8e4
Daniel <fiend.of.sound@gmail.com>
2022-12-26 16:45:01 +0400

    chore: cleaner code

--- js/anim.js
+++ js/anim.js
@@ -150,18 +150,3 @@ const drawLandscape = (cb) => {
 }
 
 drawLandscape()
-let isResizeLocked = false
-let resizeTimeout;
-document.getElementById("platform").style = "background-image: url(img/fr.jpeg);background-position:"+window.innerWidth/2+"px;"
-addEventListener("resize", (event) => {
-	// console.info('resize')
-	disableAnimation = true;
-	isResizeLocked = true;
-	document.getElementById("platform").style = "background-image: url(img/fr.jpeg);background-position:"+window.innerWidth/2+"px;"
-	clearTimeout(resizeTimeout)
-	resizeTimeout = null
-	resizeTimeout = setTimeout(()=>{
-		drawLandscape()
-		isResizeLocked = false;
-	}, 100)
-})
